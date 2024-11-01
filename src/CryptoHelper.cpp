8ba36e070f0d52356925944275b1c63a025cc5bf
josephwehby <josephw31@hotmail.com>
2024-11-01 11:53:55 -0500

    bug when adding to the totient if e is negative. probablt problem with bigint library

--- src/CryptoHelper.cpp
+++ src/CryptoHelper.cpp
@@ -1,3 +1,33 @@
 #include "CryptoHelper.hpp"
 
+bool cryptohelper::isPrime(const BigInt& a, unsigned int k) {
+  return false;
+}
 
+BigInt cryptohelper::ExtendedEuclidian(const BigInt& e, const BigInt& totient) {
+  BigInt quotient = BigInt(1);
+  BigInt a, b, remainder, t;
+  BigInt x = BigInt(0);
+  BigInt y = BigInt(1);
+
+  if (e > totient) {
+    a = e;
+    b = totient;
+  } else {
+    a = totient;
+    b = e;
+  }
+
+  while (true) {
+    remainder = a % b;
+    quotient = a / b;
+    t = x - (y * quotient);
+    a = b;
+    b = remainder;
+    x = y;
+    y = t;
+    if (remainder == 0) break;
+  }
+
+  return x;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,12 @@
 
 int main() {
   std::cout << "crypto helper function" << std::endl;
+  
+  
+  BigInt r = cryptohelper::ExtendedEuclidian(17, 120);
+
+  BigInt e = r + 120;
+  std::cout << r << std::endl;
+  std::cout << e << std::endl;
   return 0;
 }
